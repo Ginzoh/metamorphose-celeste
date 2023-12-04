3099dd36ec362a3deca049e75dd8e4b4b66441f3
Mickael <mickael.krauth@epita.fr>
2023-12-04 15:39:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-gray-500 mx-20 text-lg'>Hermes</h1>
+      <p>Informations</p>
+    </>
+  )
+}
+
+export default App
